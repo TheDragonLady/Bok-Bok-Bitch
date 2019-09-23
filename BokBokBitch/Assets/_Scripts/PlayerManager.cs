b1abd469f03b768a748b2e8fbf60c207fd0f4284
Drina <drinadomandiola@gmail.com>
2019-09-23 19:13:33 +1200

    SomethingHappenedhahaha

--- BokBokBitch/Assets/_Scripts/PlayerManager.cs
+++ BokBokBitch/Assets/_Scripts/PlayerManager.cs
@@ -1,69 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(CharacterController))]
+//[RequireComponent(typeof(CharacterController))]
 public class PlayerManager : MonoBehaviour
-{
+{ CharacterController characterController;
     public float walkspeed;
-    private bool limitDiagonalSpeed = true;
     public float Jumpspeed;
-    public float grevity;
-    public bool Aircontrol = true;
-    private Vector3 moveDirection=Vector3.zero;
-    private bool grounded = false;
-    private CharacterController controller;
-    private Transform myTransform;
-    private float speed;
-    private float slideLimit;
-    private RaycastHit hit;
-    private float rayDistance;
-    private Vector3 contactPoint;
-    private int jumpTimer;
-    private int AntibunnyhopFactor = 1;
-    private bool playerControl = false;
-    // Start is called before the first frame update
+    public float gravity;
+    public float speed;
+    private Vector3 moveDirection = Vector3.zero;
+
     void Start()
     {
-        controller = GetComponent<CharacterController>();
-        myTransform = transform;
-        speed = walkspeed;
-        rayDistance = controller.height * .5f + controller.radius;
-        jumpTimer = AntibunnyhopFactor;
-     }
-
-    private void FixedUpdate()
-    {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-        float inputModifyFactor = (inputX != 0.0f && inputY != 0.0f && limitDiagonalSpeed) ? .7071f : 1.0f;
-
-        if (!Input.GetButton("Jump"))
-            jumpTimer++;
-        else if (jumpTimer >= AntibunnyhopFactor)
-        {
-            moveDirection.y = Jumpspeed;
-            jumpTimer = 0;
-        }
-        if (Aircontrol&& playerControl)
-        {
-            moveDirection.x = inputX * speed * inputModifyFactor;
-            moveDirection.z = inputY * speed * inputModifyFactor;
-            moveDirection = myTransform.TransformDirection(moveDirection);
-
-        }
-
-        moveDirection.y -= grevity * Time.deltaTime;
-        grounded = (controller.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;
+        characterController = GetComponent<CharacterController>();
 
     }
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    private void Update()
     {
-        contactPoint = hit.point;
-    }
-   
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = Jumpspeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
